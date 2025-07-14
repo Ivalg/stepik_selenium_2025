8dba4dd687b9ee48375d91ede8cfa00939205daf
Ivalga <pro102@list.ru>
2025-07-14 06:21:11 +0300

    started implementing main_page.py and changed the first test to agebject

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,18 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
 
 
-def go_to_login_page(driver):
-    login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+# def go_to_login_page(driver):
+#     login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
+#     login_link.click()
 
 
 def test_guest_can_go_to_login_page(driver):
-    driver.get(link)
-    go_to_login_page(driver)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, link)  # передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 # def test_guest_can_go_to_login_page(driver):
 #     link = "http://selenium1py.pythonanywhere.com/"
