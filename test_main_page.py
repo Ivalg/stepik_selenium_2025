25d16951c1f1ed4a3a29749cb87eb7dc497a9267
Ivalga <pro102@list.ru>
2025-07-14 07:20:07 +0300

    added metod 'is_element_present' and second test 'test_guest_should_see_login_link'

--- test_main_page.py
+++ test_main_page.py
@@ -2,17 +2,19 @@ from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 
 
-
-# def go_to_login_page(driver):
-#     login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
-#     login_link.click()
-
-
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, link)  # передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, link)
+    page.open()
+    page.should_be_login_link()
+
 
 # def test_guest_can_go_to_login_page(driver):
 #     link = "http://selenium1py.pythonanywhere.com/"
