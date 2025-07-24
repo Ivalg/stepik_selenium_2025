d1439a999a41a9c391f8593a551e9baa980fb131
Ivalga <pro102@list.ru>
2025-07-24 07:02:58 +0300

    added class testloginfrommainpage for guest on main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,29 +1,31 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page(driver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(driver, link)  # передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(driver, driver.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, driver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(driver, link)  # передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(driver, driver.current_url)
+        login_page.should_be_login_page()
 
-
-def test_guest_should_see_login_link(driver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(driver, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, driver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(driver, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_should_see_view_basket_button(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, link)
     page.open()
-    page.should_be_view_basket_button()
+    page.should_be_basket_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(driver):
